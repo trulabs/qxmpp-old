1dbbdf4058ee08ccb991b7698dedbd0e4d2f2176
Sam Truscott <sam@wumpus.co.uk>
2014-01-31 09:52:56 +0000

    Code clean up.

--- src/base/QXmppMessage.cpp
+++ src/base/QXmppMessage.cpp
@@ -30,9 +30,6 @@
 #include "QXmppMessage.h"
 #include "QXmppUtils.h"
 
-#include <QtDebug>
-
-
 static const char* chat_states[] = {
     "",
     "active",
@@ -69,7 +66,7 @@ static const char *ns_xhtml = "http://www.w3.org/1999/xhtml";
 enum StampType
 {
     LegacyDelayedDelivery,  // XEP-0091: Legacy Delayed Delivery
-    DelayedDelivery,        // XEP-0203: Delayed Delivery
+    DelayedDelivery         // XEP-0203: Delayed Delivery
 };
 
 class QXmppMessagePrivate : public QSharedData
